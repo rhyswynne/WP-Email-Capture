c3e188456ce7028d6ae6a4fde67355f33b7ebb96
Rhys <rhys@rhyswynne.co.uk>
2015-01-31 15:18:33 +0000

    Refactored security.php

--- inc/security.php
+++ inc/security.php
@@ -4,11 +4,11 @@
 
 /* Check for injection characters */
 
-function wp_email_injection_chars($s = "") {
+/* function wp_email_injection_chars($s = "") {
 
 	return (stripos("\r", $s) || stripos("\n", $s) || stripos("%0a", $s) || stripos("%0d", $s)) ? TRUE : FALSE;
 
-}
+} */
 
 
 
@@ -16,20 +16,19 @@ function wp_email_injection_chars($s = "") {
 
 /* Make output safe for the browser */
 
-function wp_email_capture_bsafe($input = "") {
+/* function wp_email_capture_bsafe($input = "") {
 
 	return htmlspecialchars(stripslashes($input));
 
-}
+} */
 
 
 
-
-
-
-
-
-
+/**
+ * Strip Slashes from the string
+ * @param  string $s the string to use.
+ * @return string    The string cleaned of slashes.
+ */
 function wp_email_stripslashes($s = "") {
 
 	if (defined('TEMPLATEPATH') || (get_magic_quotes_gpc())) {
@@ -47,7 +46,11 @@ function wp_email_stripslashes($s = "") {
 
 
 
-
+/**
+ * Avoid injection tests
+ * @param  string $str the string to check
+ * @return string      the cleaned string
+ */
 function wp_email_injection_test($str = "") { 
 
 	$tests = array("/bcc\:/i", "/Content\-Type\:/i", "/Mime\-Version\:/i", "/cc\:/i", "/from\:/i", "/to\:/i", "/Content\-Transfer\-Encoding\:/i"); 
